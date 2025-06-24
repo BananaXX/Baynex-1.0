3a4b301a1969c1d52c9149189624f639a5559121
BananaXX <60035896+BananaXX@users.noreply.github.com>
2025-06-24 17:20:22 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,17 @@
 
+import React, { useEffect, useState } from "react";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import { auth } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
+export default function App() {
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (u) => setUser(u));
+    return unsub;
+  }, []);
+
+  return user ? <Dashboard /> : <Login />;
+}
