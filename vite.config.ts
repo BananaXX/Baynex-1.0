722c2df6fb7436b6d3cc057aa4bba6e66a077190
BananaXX <60035896+BananaXX@users.noreply.github.com>
2025-06-24 22:18:32 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,10 +5,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    host: '0.0.0.0',
   },
   preview: {
+    host: '0.0.0.0',
     port: 4173,
-    allowedHosts: ['baynex-1-0-web1.onrender.com'],
-    host: true, // ensures it works with 0.0.0.0
+    allowedHosts: ['baynex-1-0-web1.onrender.com'], // 👈 This line allows Render to show your site
   },
 });
