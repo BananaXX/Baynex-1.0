542a8a1ef35f580b4bc31fb65a0ecc9adc72e9b0
BananaXX <60035896+BananaXX@users.noreply.github.com>
2025-06-25 11:20:30 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import Login from './Login';
-import Dashboard from './Dashboard';
+import Dashboard from './components/Dashboard';
 import './firebase';
 
 const App: React.FC = () => {
